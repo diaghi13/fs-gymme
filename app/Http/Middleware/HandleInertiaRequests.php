af9d993b5acf87addd6f30c08719aae40519b61a
Davide Donghi <davide.d.donghi@gmail.com>
2025-12-07 02:04:44 +0100

    Try fix 502 on deployment server

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -120,8 +120,12 @@ class HandleInertiaRequests extends Middleware
                     return $userData;
                 } : null,
             ],
+            // TEMPORARY: Testing if Ziggy is causing 502 errors
+            // 'ziggy' => fn (): array => [
+            //     ...(new Ziggy)->toArray(),
+            //     'location' => $request->url(),
+            // ],
             'ziggy' => fn (): array => [
-                ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
